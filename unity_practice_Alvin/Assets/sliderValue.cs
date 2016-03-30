32ee8b816c6c858d3bdefee845de1b53b3bd2e4b
Alvin Herawan <alvin.h@oluss.com>
2016-03-31 00:30:50 +0800

    Add slider inside scrolling interface
    Create slider and value display.

---
+++ unity_practice_Alvin/Assets/sliderValue.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class sliderValue : MonoBehaviour {
+
+	string sliderTextString = "0";
+	public Text sliderText;
+
+	void Start (){
+
+		sliderText = gameObject.GetComponent<Text>();
+
+		sliderText.text = sliderTextString + "%";
+
+	}
+
+	public void textUpdate (float textUpdateNumber)
+	{
+		sliderTextString = textUpdateNumber.ToString();
+		sliderText.text = sliderTextString + "%";
+	}
+}
