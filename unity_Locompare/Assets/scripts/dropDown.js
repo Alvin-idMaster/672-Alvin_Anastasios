b34893cf1cdab5199dc8f40842221e84c868e9c1
Alvin Herawan <alvin.h@oluss.com>
2016-04-09 09:47:42 +0800

    Check Array Function Added
    Function to check whether the last selected location is already included
    in the array. The codes need a lot of cleanings.

--- unity_Locompare/Assets/scripts/dropDown.js
+++ unity_Locompare/Assets/scripts/dropDown.js
@@ -10,6 +10,8 @@ var LocName : String;
 
 var CurrentlySelected : List.< String >;
 
+var IsArrayChecked : boolean;
+
 function Start () {
 
 	//CurrentlySelected = new Array ();
@@ -17,6 +19,8 @@ function Start () {
 	SelectedLocation = LocationPickerLabel.GetComponent(UI.Text);
 	LocName = SelectedLocation.text;
 	SelectedLocation.text = "Select a location ...";
+
+	IsArrayChecked = false;
 
 	//CurrentlySelected.length = 0;
 }
@@ -27,13 +31,35 @@ function Update () {
 	//Debug.Log(LocationPicker.GetComponent.<UI.Dropdown>().OptionData().text);
 	//Debug.Log(LocationPicker.GetComponent.<UI.Dropdown>().value);
 
+	if(Input.GetMouseButtonDown(1)){
+		if (CurrentlySelected.Contains ("Singapore")){
+			Debug.Log("Singapore dah ada!");
+			CurrentlySelected.Remove ("Singapore");
+		}else Debug.Log("Singapore belom ada!");
+	}
+
 }
 
 function PushLocation (){
+
 	LocName = SelectedLocation.text;
+
 	Debug.Log("Country " + LocName + " will be listed.");
 
-	CurrentlySelected.Add (LocName);
+	if (CurrentlySelected.Count < 2) {
+
+		CurrentlySelected.Add (LocName);
+
+	}
+
+	if (CurrentlySelected.Count >= 2){
+
+		//var LatestLocation : String = SelectedLocation.text;
+		CheckLocations();
+
+		if(IsArrayChecked)CurrentlySelected.Add(LocName);
+    }
+}
 
 	Debug.Log(CurrentlySelected.Count);
 
@@ -41,9 +67,45 @@ function PushLocation (){
 	if (CurrentlySelected.Length == 0){
 		CurrentlySelected = new String [1];
 		CurrentlySelected[0] = LocName;
+	}*/
+
+function CheckLocations(){
+
+	IsArrayChecked = false;
+
+	var i = 0;
+
+	while (i < CurrentlySelected.Count) {
+
+		if (CurrentlySelected[i] == LocName){
+			IsArrayChecked = false;
+			return;
+		}
+
+		if (CurrentlySelected[i] != LocName) {
+			i++;
+		}
 	}
 
-	for (var i = 0; i < CurrentlySelected.Count; i++) {
-         GUI.Label(Rect(0, 0+(i*30), 200, 30), names[i]);
-     }*/
+	if (i >= CurrentlySelected.Count){
+		IsArrayChecked = true;
+	}
+
+	/*
+	for (var i = 0; i < CurrentlySelected.Count; i += 0) {
+
+	Debug.Log("Checking if " + CurrentlySelected[i] + " is already listed.");
+
+		if (CurrentlySelected[i] == LocName){
+			Debug.Log("Same location detected.");
+			IsArrayChecked = true;
+			return;
+		}
+
+		if (CurrentlySelected[i] != LocName){
+			IsArrayChecked = false;
+			i++;
+		}
+	}*/
+
 }
