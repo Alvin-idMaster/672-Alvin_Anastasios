3a0159fa43d183d2cb448617aea3df24c627ea21
Alvin Herawan <alvin.h@oluss.com>
2016-04-19 08:09:39 +0800

    Addressing Issue #12
    Adding new code for a child under the Dropdown object to set the height
    of the drawer to be as tall as the screen.

---
+++ unity_Locompare/Assets/scripts/dropDown_height.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+import UnityEngine.UI;
+
+private var DrawerHeight : RectTransform;
+
+function Start () {
+
+	DrawerHeight = GetComponent(RectTransform);
+
+	DrawerHeight.sizeDelta = Vector2 (0, Screen.height);
+
+}
+
+function Update () {
+
+}
