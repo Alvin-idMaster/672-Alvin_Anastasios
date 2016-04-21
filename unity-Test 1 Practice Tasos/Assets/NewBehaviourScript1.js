cd387d786e99bd38a1aa928bf9a721ab45c034d5
anastasiosioannou <anastasios.ioannou@idmaster.eu>
2016-04-21 22:55:21 +0300

    Upload test 1 Project
    Button exercise and fuction

---
+++ unity-Test 1 Practice Tasos/Assets/NewBehaviourScript1.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+import UnityEngine.UI;
+
+var DisplayText:Text;
+
+function Start () {
+
+DisplayText=GetComponent(UI.Text);
+
+}
+
+function Update () {
+
+}
+
+function SayHello(){
+
+DisplayText.text="hello";
+
+}
