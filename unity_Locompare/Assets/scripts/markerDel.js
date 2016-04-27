b210a68b1a2f011a90269c3e58a6197b15756375
Alvin Herawan <alvin.h@oluss.com>
2016-04-27 09:22:00 +0800

    A Little Experiment on Issue #10
    See issue page for more details.

--- unity_Locompare/Assets/scripts/markerDel.js
+++ unity_Locompare/Assets/scripts/markerDel.js
@@ -6,19 +6,13 @@ private var myRect : RectTransform;
 function Start () {
 
 	myRect = GetComponent(RectTransform);
-	/*
-	//After tested on the editor, it is set that the size of the RectTransform is 46(width) and 30(height)
-	myRect.offsetMin.x = Random.Range(0.0, 365.0);
-	myRect.offsetMax.y = Random.Range(0.0, 195.0);
-	myRect.offsetMax.x = 408 - 46 - myRect.offsetMin.x;
-	myRect.offsetMin.y = 195 - myRect.offsetMax.y;
-	*/
+
 }
 
 function Update () {
 
-	//Debug.Log(myRect.offsetMin);	//offsetMin.x = left limit, range 0 until 365
-	//Debug.Log(myRect.offsetMax);	//offsetMax.y = Top limit, range 0 until -194
-	//Debug.Log(myRect.sizeDelta); 	//-365, -195
+}
 
+function DeleteMarker (){
+	if(Input.GetMouseButton(0)) Debug.Log("I'm going to be deleted!");
 }
